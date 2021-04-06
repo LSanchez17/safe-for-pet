81c4e189425ca3e7436a631ad98630d1b51b7be8
LSanchez17 <galaxyplayeremail@gmail.com>
2021-04-06 10:40:49 -0400

    set up for dashboards

---
+++ src/helpers/loggingApi.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3001';
+
+class loggingApi {
+    static async logVisit() {
+        try{
+
+        }
+        catch(e){
+
+        }
+    }
+}
+
+export default loggingApi;
